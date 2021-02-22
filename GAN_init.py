5a20fec89b3b186379398f75a1f25cf70743f340
poojith28 <45728745+poojith28@users.noreply.github.com>
2021-02-22 18:40:53 +0530

    Add files via upload

--- GAN_init.py
+++ GAN_init.py
@@ -1,8 +1,23 @@
 
 from GAN_Data_Prep import Data_prep
-
-Model = Data_prep(DATA_Prep = True, Sample_data_prop = 100, GAN_data_path=None)
-
-data = Model.model()
+from sdv.constraints import UniqueCombinations
+from sdv.tabular import TVAE
 
 
+model = Data_prep(DATA_Prep = True, Sample_data_prop = 100, GAN_data_path=None)
+# Data prep for Gan model
+data = model.model()
+
+# forming the constraints
+unique_Train_constraint = UniqueCombinations(columns=['Fromstation', 'Tostation','TrainNo'], handling_strategy='reject_sampling')
+constraints = [unique_Train_constraint]
+
+# defining a model
+model = TVAE(constraints= constraints)
+
+# training
+model.fit(data)
+
+# sample genration
+num_samples = 1000000
+samples = model.sample(num_samples)
